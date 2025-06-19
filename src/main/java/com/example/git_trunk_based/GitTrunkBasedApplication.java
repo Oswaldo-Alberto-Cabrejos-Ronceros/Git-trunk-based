321691a606176408b1b75fb28bd8529ec3402162
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-18 19:44:18 -0500

    first commit

---
+++ src/main/java/com/example/git_trunk_based/GitTrunkBasedApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_trunk_based;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrunkBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrunkBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_trunk_based/GitTrunkBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_trunk_based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrunkBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
